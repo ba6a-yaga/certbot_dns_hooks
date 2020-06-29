055e987f417773a88595f6b426fca962d2358bda
x3mprog <x3mprog@gmail.com>
2020-06-29 12:29:12 +0300

    - [x] Добавлена обработка субдомена

--- hooks/cloudflare_auth_hook.rb
+++ hooks/cloudflare_auth_hook.rb
@@ -3,51 +3,57 @@
 require_relative '/usr/lib/request.rb' 
 
 SUBDOMAIN = ENV["SUBDOMAIN"].nil? ? "_acme-challenge" : ENV["SUBDOMAIN"]
-
+domains = ENV["CERTBOT_DOMAIN"].split('.')
+main_domain = "#{domains[domains.size-2]}.#{domains[domains.size-1]}"
 response_get_id_zone = Request.new { |r|
     r.headers = {
         Authorization: "Bearer #{ENV["Token"]}",
         'Content-Type' => 'application/json' 
     }
-    r.uri = "https://api.cloudflare.com/client/v4/zones?name=#{ENV["CERTBOT_DOMAIN"]}"
+    r.uri = "https://api.cloudflare.com/client/v4/zones?name=#{main_domain}"
     r.method = :get
 }.fetch
+p response_get_id_zone
 
-response_add_subdomain = Request.new { |r|
-    r.headers = {
-        Authorization: "Bearer #{ENV["Token"]}",
-        'Content-Type' => 'application/json' 
-    }
-    r.uri = "https://api.cloudflare.com/client/v4/zones/#{response_get_id_zone['result'][0]['id']}/dns_records"
-    r.method = :post
-    r.data = {
-        type: 'TXT',
-        name: SUBDOMAIN,
-        content: ENV["CERTBOT_VALIDATION"],
-        ttl: 120
-    }
-}.fetch
 
-p response_add_subdomain
+if response_get_id_zone['success']
+    response_add_subdomain = Request.new { |r|
+        r.headers = {
+            Authorization: "Bearer #{ENV["Token"]}",
+            'Content-Type' => 'application/json' 
+        }
+        r.uri = "https://api.cloudflare.com/client/v4/zones/#{response_get_id_zone['result'][0]['id']}/dns_records"
+        r.method = :post
+        r.data = {
+            type: 'TXT',
+            name: SUBDOMAIN,
+            content: ENV["CERTBOT_VALIDATION"],
+            ttl: 120
+        }
+    }.fetch
+    p response_add_subdomain
 
-if response_add_subdomain['success']
-    puts "DNS settings successfully changed"
-    path = "/tmp/#{ENV["CERTBOT_DOMAIN"]}"
-    Dir.mkdir path unless Dir.exist? path 
-    File.open("#{path}/#{SUBDOMAIN}_RECORD_ID", 'w+') { |f| f.puts response_add_subdomain['result']['id']}
-    File.open("#{path}/#{SUBDOMAIN}_ZONE_ID", 'w+') { |f| f.puts response_get_id_zone['result'][0]['id']}
-
-    120.times do
-        result = `host -t TXT #{SUBDOMAIN}.#{ENV["CERTBOT_DOMAIN"]}`
-        result_txt = result.match /#{ENV["CERTBOT_VALIDATION"]}/
-
-        if result_txt.to_a[0] == ENV["CERTBOT_VALIDATION"]
-            puts "DNS settings applied"
-            return
-        else
-            puts "Waiting for a DNS change..."
-            sleep 10
+    if response_add_subdomain['success']
+        puts "DNS settings successfully changed"
+        path = "/tmp/#{ENV["CERTBOT_DOMAIN"]}"
+        Dir.mkdir path unless Dir.exist? path 
+        File.open("#{path}/#{SUBDOMAIN}_RECORD_ID", 'w+') { |f| f.puts response_add_subdomain['result']['id']}
+        File.open("#{path}/#{SUBDOMAIN}_ZONE_ID", 'w+') { |f| f.puts response_get_id_zone['result'][0]['id']}
+    
+        120.times do
+            result = `host -t TXT #{SUBDOMAIN}.#{ENV["CERTBOT_DOMAIN"]}`
+            result_txt = result.match /#{ENV["CERTBOT_VALIDATION"]}/
+    
+            if result_txt.to_a[0] == ENV["CERTBOT_VALIDATION"]
+                puts "DNS settings applied"
+                return
+            else
+                puts "Waiting for a DNS change..."
+                sleep 10
+            end
         end
+    else
+        puts "Something went wrong..."
     end
 else
     puts "Something went wrong..."
